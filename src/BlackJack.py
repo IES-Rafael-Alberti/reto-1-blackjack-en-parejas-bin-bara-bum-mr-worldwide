35f73d00ce1854e13e4a901e4b4017e2971f6beb
Sara Sanchez <ssancam391@g.educaand.es>
2023-11-13 22:37:04 +0100

    comentarios_en_main_BlackJack

--- src/BlackJack.py
+++ src/BlackJack.py
@@ -4,12 +4,15 @@ def jugar_black_Jack():
     
     #Importamos todas las funciones que vamos a necesitar para el desarrollo del juego:
     from src.eleccion_jugador import modo_juego
+    from src.mano import mano_inicial
     from src.obt_carta import obtener_carta
     from src.valor_carta import puntuacion_carta
-    from src.mano import mano_inicial
+    
+    #//////////////////////////\\\\\\\\\\\\\\\\\\\\\\\#
     try:
-        #####PRINT MENSAJE DE BIENVENIDA
-        #Iniciación del juego
+        #####PRINT MENSAJE DE BIENVENIDA#####
+        
+        #INICIO DEL JUEGO
         print("Introduce tu nombre: ")    
         jugador_1 = input()
         
@@ -28,15 +31,15 @@ def jugar_black_Jack():
         else:
             raise ValueError ("**ERROR**\nDebes introducir una de las dos opciones.")
         
-        print(mano_inicial(jugador_1))
         
-        print(mano_inicial(jugador_2))
         #INICIALIZAR LA MANO 
-            #INICIALIZAR PARA CADA JUGADOR 
-            
-        #AGREGAR CARTAS A LA MANO INICIAL     
-                  
+        #Iniciamos la partida repartiendo dos cartas por jugador. 
+        print(mano_inicial(jugador_1))
+        print(mano_inicial(jugador_2))
+        #---HAY QUE MOSTRAR LA MANO A CADA JUGADOR Y REGUNTAR SI QUIEREN MAS CARTAS O SE PLANTAN---#
+         
         #RONDAS
+            #---HAY QUE INCLUIR LA PUNTUACION DE LA MANO INICIAL---#
             #RESULTADOS RONDA
             #CALCULAR RESULTADOS
         
